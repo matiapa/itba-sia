984447d6a021fe0e07350ba5c78a6acfe5c2de3f
Leonardo <ldagostino@itba.edu.ar>
2022-03-11 14:28:13 -0300

    animations

--- front.py
+++ front.py
@@ -1,5 +1,7 @@
 import pygame
 import pygame_gui
+import random
+import time
 
 BABY_BLUE = 191, 215, 237
 NAVY_BLUE = 0, 59, 115
@@ -22,16 +24,16 @@ solve_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((800, 600)
                                              text='Solve',
                                              manager=manager,
                                              object_id="#solve_btn")
-
 random_button = pygame_gui.elements.UIButton(relative_rect=pygame.Rect((135, 600), (350, 70)),
                                              text='Random',
                                              manager=manager,
                                              object_id="#solve_btn")
+
 # Busqueda (label & dropdown)                               
 dropdown_layout_rect = pygame.Rect((850, 150), (280, 60))
 algorithmOptions = ["BPA", "BPP", "BPPV", "Heuristica Local", "Heuristica Global","A*"]
 algorithmDropDown = pygame_gui.elements.UIDropDownMenu(options_list=algorithmOptions,
-                                                       starting_option=algorithmOptions[1],
+                                                       starting_option=algorithmOptions[0],
                                                        relative_rect=dropdown_layout_rect,
                                                        manager=manager)
 pygame_gui.elements.ui_label.UILabel(parent_element=algorithmDropDown,
@@ -43,7 +45,7 @@ pygame_gui.elements.ui_label.UILabel(parent_element=algorithmDropDown,
 dropdown_layout_rect2 = pygame.Rect((850, 270), (280, 60))            
 heuristicsOptions = ["Manhattan Distance", "Misplaced Tiles", "Cuadratic Manhattan"]
 heuristicsDropDown = pygame_gui.elements.UIDropDownMenu(options_list=heuristicsOptions,
-                                                       starting_option=heuristicsOptions[1],
+                                                       starting_option=heuristicsOptions[0],
                                                        relative_rect=dropdown_layout_rect2,
                                                        manager=manager)
 pygame_gui.elements.ui_label.UILabel(parent_element=heuristicsDropDown,
@@ -61,9 +63,45 @@ def draw_blocks():
             textRect.center = rect.left+60, rect.top+60
             screen.blit(textSurf, textRect)
 
+def randomize():
+    random.shuffle(grid)
+
+def solve(config):
+    # config.algorithm & config.heuristic
+    
+    animate(ret)
+    return True
+
+def display(grid):
+    for i in range(3):
+        for j in range(3):
+            rect = pygame.Rect(i*15 + (i+1)*120, j*15 + (j)*120 + 150, 120, 120)
+            pygame.draw.rect(screen, NAVY_BLUE, rect)
+            textSurf = font.render(str(grid[j][i]), True, WHITE)
+            textRect = textSurf.get_rect()
+            textRect.center = rect.left+60, rect.top+60
+            screen.blit(textSurf, textRect)
+    pygame.display.update()
+
+def animate(grids):
+    for grid in grids:
+        display(grid)
+        time.sleep(0.5)
+
 screen.blit(background, (0, 0))
 pygame.display.update()
 clock = pygame.time.Clock()
+algorithm = algorithmOptions[0]
+heuristic = None
+ret = [
+        [[1,2,3],[5,6,0],[4,7,8]],
+        [[1,2,3],[5,0,6],[4,7,8]],
+        [[1,2,3],[0,5,6],[4,7,8]],
+        [[1,2,3],[4,5,6],[0,7,8]],
+        [[1,2,3],[4,5,6],[7,0,8]],
+        [[1,2,3],[4,5,6],[7,8,0]],
+    ]
+grid = ret[0]
 
 crashed = False
 while not crashed:
@@ -73,6 +111,18 @@ while not crashed:
         if event.type == pygame.QUIT:
             crashed = True
         
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == solve_button:
+                    solve(None)
+                elif event.ui_element == random_button:
+                    randomize()
+
+            elif event.user_type == pygame_gui.UI_DROP_DOWN_MENU_CHANGED:
+                if event.ui_element == algorithmDropDown:
+                    algorithm = event.text
+                elif event.ui_element == heuristicsDropDown:
+                    heuristic = event.text
         manager.process_events(event)
     
         
@@ -80,5 +130,5 @@ while not crashed:
     screen.blit(background, (0, 0))
     screen.blit(title, (350, 35))
     manager.draw_ui(screen)
-    draw_blocks()
+    display(grid)
     pygame.display.update()
