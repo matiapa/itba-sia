4aeacf7e52294c0ccdfff98c70571789bbdd2a27
Leonardo <ldagostino@itba.edu.ar>
2022-03-24 20:20:42 -0300

    mutation implemented

--- TP2/main/mutation.py
+++ TP2/main/mutation.py
@@ -1,4 +1,5 @@
 from individual import Individual
+import numpy as np
 
 class Mutation:
 
@@ -6,5 +7,9 @@ class Mutation:
     Alters some genes with a certain probability and within certain range.
     This introduces more genetic variety on the population.
     """
-    def apply(self, individual: Individual) -> None:
-        raise NotImplementedError()
+    def apply(self, individual: Individual, p: float, sigma: float) -> Individual:
+        for i in range(len(individual.genes)):
+            if np.random.uniform(0, 1) < p:
+                individual.genes[i] += np.random.normal(0, sigma)
+        
+        return individual
