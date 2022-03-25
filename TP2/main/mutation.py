768631f2f035111999e98f88ee577e45395ff816
Leonardo <ldagostino@itba.edu.ar>
2022-03-24 21:37:44 -0300

    concrete mutation classes

--- TP2/main/mutation.py
+++ TP2/main/mutation.py
@@ -7,9 +7,23 @@ class Mutation:
     Alters some genes with a certain probability and within certain range.
     This introduces more genetic variety on the population.
     """
+    def apply(self, individual: Individual) -> None:
+        raise NotImplementedError()
+
+class SampleMutation(Mutation):
+
     def apply(self, individual: Individual, p: float, sigma: float) -> Individual:
         for i in range(len(individual.genes)):
             if np.random.uniform(0, 1) < p:
                 individual.genes[i] += np.random.normal(0, sigma)
         
         return individual
+
+class BagMutation(Mutation):
+    
+        def apply(self, individual: Individual, p: float) -> Individual:
+            for i in range(len(individual.genes)):
+                if np.random.uniform(0, 1) < p:
+                    individual.genes[i] = 1 if individual.genes[i] == 0 else 0
+            
+            return individual
