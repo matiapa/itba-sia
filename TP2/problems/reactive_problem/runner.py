1cc985c07abbf197708fcd1014febe0638c52aa0
Leonardo <ldagostino@itba.edu.ar>
2022-03-28 00:23:17 -0300

    Printing F and E values

--- TP2/problems/reactive_problem/runner.py
+++ TP2/problems/reactive_problem/runner.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("..")
 sys.path.append("../..")
 
-from fitness import ReactiveFitness
+from fitness import ReactiveFitness, F, inputs
 from individual import ReactiveIndividual, ReactiveIndividualFactory
 
 from typing import List
@@ -13,6 +13,7 @@ from main.mutation import UniformIntegerMutation
 from main.pairing.elitist_pairing import ElitistPairing
 from main.selection.elite_selection import EliteSelection
 from main.selection.roulette_selection import RouletteSelection
+
 
 from main.algorithm import Algorithm
 
@@ -28,8 +29,11 @@ iterator = iter(algorithm)
 t = 0
 while t < 10:
     individuals : List[ReactiveIndividual] = next(iterator)
-
+    print("\n", end="\n")
+    print(f"Generation {t}", end="\n")
     for i in individuals:
         print(i, end='\n')
+        print("Fitness aka AntiError: ", ReactiveFitness().apply(i), end="\n")
+        print("Funcion F: ", F(i.W, i.w, i.w_0, inputs["xi"][0]), end="\n")
 
     t += 1
