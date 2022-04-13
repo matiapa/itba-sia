dc0f1146f58dd36cc476c3462c9dfb381379239c
Gonza Beade <gbeade@itba.edu.ar>
2022-04-13 03:49:52 -0300

    Anda bien para perceptrones simples y lineales, el resto falta seguir indagando

--- TP3/container.py
+++ TP3/container.py
@@ -1,30 +1,79 @@
+from turtle import ycor
 from layer import * 
+import numpy as np 
 
 class Container: 
 
-    def __init__(self, *args: Layer): 
-        self.layers = args
+    losses = {
+        "quadratic": [ lambda y, a: sum(np.square(np.subtract(y, a))), lambda y, a: [[-2*(y_i-a_i)] for (y_i, a_i) in zip(y, a)]], # TODO: check 
+    }
 
-    def __call__(self, input: np.ndarray): 
+    def __init__(self, loss_fun: str, *args: Layer): 
+        self.loss = Container.losses[loss_fun] 
+        self.layers = args
+        
+    def __call__(self, input: np.ndarray, expected_output: np.ndarray, train: bool):
+        res = self.consume(input)
+        if len(res) != len(expected_output): 
+            raise Exception('Something went wrong: len(expected_output) != len(res)')
+        if train:
+            self.backpropagation(res, expected_output) 
+        return res, self.loss[0](expected_output, res)
+
+    def consume(self, input: np.ndarray):
         res = input
         for layer in self.layers: 
             res = layer(res)
-        return res 
+        return res
 
+    def backpropagation(self, actual_output: np.ndarray, expected_output: np.ndarray): 
+        delta = np.array(self.loss[1](expected_output, actual_output))
+        # print("DELTA_0", delta) # ok 
+        for layer in self.layers[::-1]: 
+            delta = layer.update(delta)
 
-# TODO: me da numeros un poco grandes, me gustaria estudiarlo un poco mas 
-# Supongo que pasa porque las capas son demasiado largas (elegi 30, 40, 50, 10)
-# Con capas mas chicas me da mejor 
-# La logica multiplicativa me da o 
-
-x = Container(
+container = Container(
+    "quadratic", 
+    DenseNoBiasLayer(2, activation="id"), 
+    DenseNoBiasLayer(2, activation="id"), 
     DenseNoBiasLayer(1, activation="sigmoid"), 
-    DenseNoBiasLayer(2, activation="sigmoid"), 
-    DenseNoBiasLayer(3, activation="sigmoid"),
-    DenseNoBiasLayer(6, activation="id")
 )
 
-res = x(np.random.rand(3))
-print(len(res))
-print(res)
+# t = [ [1, 1], [1, -1], [-1, -1], [-1, 1]]
+# expected = [ [1], [0], [0], [1] ]
+
+# t = [ [1, 1], [1, 2], [3, 4], [-1, -1], [-3, -2]]
+# expected = [ [1], [1], [1], [-1], [-1]]
+
+t = []
+expected = [] 
+for k in range(1000): 
+    p = np.random.uniform(-1, 1, (2))
+    t.append(p)
+    expected.append([1 if p[0]*p[0]+p[1]*p[1] < 1 else 0]) 
+    # expected.append([1 if p[1] > 3*p[0]+1 else 0])
+    # print(p, 1 if p[0]*p[0]+p[1]*p[1] < 1 else 0)
+
+
+total_loss = 0 
+for epoch in range(10):
+    print(epoch)
+    for (s, ex) in zip(t, expected): 
+        res, loss = container(s, ex, True)
+        print(s, res, ex)
+        total_loss += loss
+
+# # print(res, "--", total_loss/100, "%")
+
+for i in range(-100, 100): 
+    for j in range(-100, 100):
+        res = container.consume([i, j]) 
+        plt.plot(i/100, j/100, 'k.' if res > 0.5 else 'r.')
+plt.show()
+
+
+
+
+
+
 
--- TP3/layer.py
+++ TP3/layer.py
@@ -5,29 +5,38 @@ from matplotlib import pyplot as plt
 class Layer: 
 
     def __init__(self):
-        self.born = False 
+        self.born = False
 
     def build(self, input_shape: int):
-        raise '\'build()\' on Layer not implemented' 
-
-    def call(self, input: np.ndarray): 
-        raise '\'call()\' on Layer not implemented'
+        pass
+    
+    def call(self, input: np.ndarray):
+        pass 
 
     def __call__(self, input: np.ndarray): 
         if not self.born: 
             self.born = True 
             self.build(len(input))
         return self.call(input) 
+    
+    def update_weights(self, delta): 
+        pass 
+    
+    def new_delta(self, delta): 
+        pass 
 
-
+    def update(self, delta): 
+        self.update_weights(delta)
+        return self.new_delta(delta)
+    
 class DenseNoBiasLayer(Layer): 
 
     activations = {
-        "relu": lambda x: np.max(0, x), 
-        "sigmoid": lambda x: 1 / (1+np.exp(-x)),
-        "tanh": lambda x: np.tanh(x), 
-        "id": lambda x: x, 
-        "step": lambda x: np.sign(x)
+        "relu": [lambda x: np.maximum(0, x), lambda x: 1 if x > 0 else 0], 
+        "sigmoid": [lambda x: 1 / (1+np.exp(-x)), lambda x: (1 / (1+np.exp(-x)))*(1-(1 / (1+np.exp(-x))))],
+        "id": [lambda x: x, lambda x: 1], 
+        "step": [lambda x: np.sign(x), lambda x: 0], 
+        "caca": [lambda x: 2*x, lambda x: 3*x]
     }
 
     def __init__(self, units: int, activation: str):
@@ -35,12 +44,39 @@ class DenseNoBiasLayer(Layer):
         self.units = units
         self.g = DenseNoBiasLayer.activations[activation]
 
+        # caches 
+        self.last_input = None 
+        self.last_z = None 
+        self.diagonal_matrix = None 
+
     def build(self, input_shape: int):
-        self.w = np.random.rand(self.units, input_shape)
+        self.w = np.random.normal( 0, 1, (self.units, input_shape) ) # Perhaps N~(0, 1) is better
     
     def call(self, input: np.ndarray):
-        print("MULTIPLYING")
-        return self.g(np.matmul(self.w, np.array(input)))
+        self.last_z = np.matmul(self.w, input)
+        self.last_input = np.array([input])
+        nonlinear =  self.g[0](self.last_z)
+        # print("Input", input)
+        # print("W", self.w)
+        # print("Z", self.last_z)
+        # print("NONLINEAR", nonlinear)
+        return nonlinear 
+
+    
+    def update_weights(self, delta: np.ndarray): 
+        self.diagonal_matrix = np.zeros((self.units, self.units))
+        # print("EMPTY DIAGONAL", self.diagonal_matrix)
+        for i in range(self.units): 
+            self.diagonal_matrix[i][i] =  (self.g)[1](self.last_z[i])
+        # print("FULL DIAGONAL", self.diagonal_matrix)
+        # print("DELTA", delta)
+        # print("LAST INPUT", self.last_input)
+        # print("OLD_W", self.w)
+        self.w = self.w - 0.1*np.array(np.matmul(np.matmul(self.diagonal_matrix, delta), self.last_input))
+        # print("NEW_W", self.w)
+    
+    def new_delta(self, delta: np.ndarray): 
+        return np.matmul(np.matmul(np.transpose(self.w), self.diagonal_matrix), delta) 
 
 
 
