c7b7c9da8b959100d4ad39baecb67111cbcdaf67
Gonza Beade <gbeade@itba.edu.ar>
2022-04-27 09:23:04 -0300

    Minor bug fixes

--- TP3/grapher.py
+++ TP3/grapher.py
@@ -14,15 +14,45 @@ def graph_simple_perceptron(container: Container, psi, zeta, i):
     plt.ylim([-square_len, square_len])
 
     # Graph metadata 
-    plt.title("AND Problem - Step activation function")
+    plt.title("XOR Problem - Step activation function")
 
     black_patch = matplotlib.patches.Patch(color='black', label='-1')
     red_patch = matplotlib.patches.Patch(color='red', label='1')
-    plt.legend(handles=[red_patch, black_patch])
+    plt.legend(handles=[red_patch, black_patch], loc='upper left')
 
     for x in range(-square_len*point_density, square_len*point_density): 
         for y in range(-square_len*point_density, square_len*point_density): 
             res = container.consume([x/point_density, y/point_density])
+            if res >= 0:
+                plt.plot(x/point_density, y/point_density, 'k.', markersize=0.25)       
+
+    for psi_mu, zeta_mu in zip(psi, zeta): 
+        plt.plot(psi_mu[0], psi_mu[1], 'rx' if zeta_mu[0] == 1 else 'ko') 
+
+    plt.savefig('TP3/out/simple_perceptron/{0}.png'.format(i))
+    plt.cla() 
+
+
+def graph_polybasis_perceptron(container: Container, psi, zeta, i): 
+    square_len = 2
+    density = 0.05
+    point_density = int(square_len / density)
+
+    # Enforce limits in axis 
+    plt.xlim([-square_len, square_len])
+    plt.ylim([-square_len, square_len])
+
+    # Graph metadata 
+    plt.title("XOR Problem - Step activation function")
+
+    black_patch = matplotlib.patches.Patch(color='black', label='-1')
+    red_patch = matplotlib.patches.Patch(color='red', label='1')
+    plt.legend(handles=[red_patch, black_patch], loc='upper left')
+
+    for x in range(-square_len*point_density, square_len*point_density): 
+        for y in range(-square_len*point_density, square_len*point_density): 
+            t = [x/point_density, y/point_density]
+            res = container.consume( [1, t[0], t[1], t[0]*t[1], t[0]**2, t[1]**2])
             if res >= 0:
                 plt.plot(x/point_density, y/point_density, 'k.', markersize=0.25)       
 
---
+++ TP3/runner_polybasis.py
@@ -0,0 +1,27 @@
+from container import * 
+from grapher import * 
+import sklearn
+
+psi = [ [1, 1], [1, -1], [-1, 1], [-1, -1]]
+zeta = [ [-1], [1], [1], [-1] ] 
+
+psi_prime = [ [1, x[0], x[1], x[0]*x[1], x[0]**2, x[1]**2] for x in psi ]
+
+
+epochs = 50
+
+for epoch in range(epochs): 
+
+    # Feed psis in random order
+    array1_shuffled, array2_shuffled = sklearn.utils.shuffle(psi, zeta)
+
+    for psi_mu, zeta_mu in zip(psi_prime, zeta): 
+        res, loss = container(psi_mu, zeta_mu, True)
+        print(res, loss)
+    
+print(container.layers[0].w)
+graph_polybasis_perceptron(container, psi, zeta, epoch)     
+
+
+
+
